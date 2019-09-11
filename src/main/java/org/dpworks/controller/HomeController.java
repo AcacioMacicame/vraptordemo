ac28df4a1617f0bda159aac4a06e9a6fe840b705
acacio.ch.m@gmail.com <acacio.ch.m@gmail.com>
2019-09-11 13:06:54 +0200

    aula sobre configuracao do projecto vraptor

---
+++ src/main/java/org/dpworks/controller/HomeController.java
@@ -0,0 +1,17 @@
+package org.dpworks.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+
+/**
+ * Hello world!
+ *
+ */
+@Controller
+public class HomeController
+{
+    @Path("/")
+    public void index(){
+
+    }
+}
